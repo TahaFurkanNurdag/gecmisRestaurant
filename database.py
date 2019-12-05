7770b31c7af9055f1a60126c91a664641ec157c3
TahaFurkanNurdag <33376340+TahaFurkanNurdag@users.noreply.github.com>
2019-12-05 23:54:06 +0300

    last tfn

--- database.py
+++ database.py
@@ -19,15 +19,7 @@ conn.execute('''CREATE TABLE `kullanicilar` (
 
 conn.execute('''CREATE TABLE `anliksiparis` (
 	`masaId` INTEGER,
-	`yemekAdi`	TEXT,
-	`yemekAdedi`	INTEGER,
-	`icecekAdi`	TEXT,
-	`icecekAdedi`	INTEGER,
-	`tatliAdi`	TEXT,
-	`tatliAdedi`	INTEGER,
-	`salataAdi`	TEXT,
-	`salataAdedi`	INTEGER,
-	`fiyat` INTEGER,
+	`urunId`	INTEGER,
 	`aciklama` TEXT,
 	PRIMARY KEY(`masaId`)
 		)''')
@@ -41,32 +33,31 @@ conn.execute('''CREATE TABLE `genelsiparis` (
 		)''')
 		
 		
-conn.execute('''CREATE TABLE `yemekler` (
-	`categoryId`	INTEGER,
+conn.execute('''CREATE TABLE `urunler` (
+	`id`	INTEGER,
 	`isim`	TEXT,
 	`fiyat` INTEGER,
-	PRIMARY KEY(`categoryId`)
+    `kategori` INTEGER,
+	PRIMARY KEY(`id`)
 		)''')
 
-conn.execute('''CREATE TABLE `tatlilar` (
-	`categoryId`	INTEGER,
-	`isim`	TEXT,
-	`fiyat` INTEGER,
-	PRIMARY KEY(`categoryId`)
+conn.execute('''CREATE TABLE `kategoriler` (
+	`id`	INTEGER,
+    `isim` INTEGER,
+	PRIMARY KEY(`id`)
 		)''')
 
-conn.execute('''CREATE TABLE `icecekler` (
-	`categoryId`	INTEGER,
-	`isim`	TEXT,
-	`fiyat` INTEGER,
-	PRIMARY KEY(`categoryId`)
-		)''')
+
 		
 		
-conn.execute('INSERT INTO kullanicilar (adi,soyadi,email,kullaniciAdi,parola,adres,tel,adminMi) VALUES ( "a","a","a@a","a","a","a",123,1)')
-
-
-
+conn.execute('INSERT INTO "main"."kullanicilar" ("userId", "adi", "soyadi", "email", "kullaniciAdi", "parola", "adres", "tel", "adminMi") VALUES (1, "Özgür", "Özbek", "ozgurozbek1@yandex.com", "Admin", "122333", "-" , "05062545050", 1)')
+conn.execute('INSERT INTO "main"."kullanicilar" ("userId", "adi", "soyadi", "email", "kullaniciAdi", "parola", "adres", "tel", "adminMi") VALUES (2, "Taha Furkan", "Nurdağ", "tfn@tfn.com","TFN", "tfn", "TFN", "05350363646", 1)')
+conn.execute('INSERT INTO "main"."urunler" ("id", "isim", "fiyat", "kategori") VALUES (1, "Makarna", "5.00", 1)')
+conn.execute('INSERT INTO "main"."urunler" ("id", "isim", "fiyat", "kategori") VALUES (2, "Baklava", "3.00", 2)')
+conn.execute('INSERT INTO "main"."urunler" ("id", "isim", "fiyat", "kategori") VALUES (3, "Su", 1.00, 3)')
+conn.execute('INSERT INTO "main"."kategoriler" ("id", "isim") VALUES (1, "Yemekler")')
+conn.execute('INSERT INTO "main"."kategoriler" ("id", "isim") VALUES (2, "Tatlilar")')
+conn.execute('INSERT INTO "main"."kategoriler" ("id", "isim") VALUES (3, "Icecekler")')
 conn.commit()
 
 conn.close()
